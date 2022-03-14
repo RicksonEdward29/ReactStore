3c12647b2817a970a7ea0992599a4ea5c30caad9
BrunoCzeck <brunoczeck@hotmail.com>
2022-03-14 20:50:46 -0300

    Alterei o titulo do arquivo app.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Testando Alterações!</Text>
+      <Text>Hello World!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f1f1',
+    backgroundColor: '#ffff',
     alignItems: 'center',
     justifyContent: 'center',
   },
